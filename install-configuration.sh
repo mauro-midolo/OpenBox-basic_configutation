8b6584ff42879b7fffd073f352d3f0db50140e6c
Mauro <mauro@AsusVX6.lan>
2020-02-01 19:48:54 +0100

    added first installation

--- install-configuration.sh
+++ install-configuration.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+#install basic application
+sudo apt-get install openbox obmenu obconf xinit terminator 
+
+#Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
