c53b51b6cafbefb3e9763cb49da950d5842556d8
Mauro <mauro@AsusVX6.lan>
2020-02-04 22:25:05 +0100

    Added wicd

--- install-configuration.sh
+++ install-configuration.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 #install basic application
-sudo apt-get install openbox obmenu obconf xinit \
-	terminator conky unzip nautilus gcc make
+sudo apt-get install -y openbox obmenu obconf xinit \
+	terminator conky unzip nautilus gcc make tint2 \
+	wicd
 
 #Install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -14,3 +15,6 @@ wget http://it.download.nvidia.com/XFree86/Linux-x86_64/340.108/NVIDIA-Linux-x86
 chmod +x NVIDIA-Linux-x86_64-340.108.run
 sudo nano ./NVIDIA-Linux-x86_64-340.108.run
 rm NVIDIA-Linux-x86_64-340.108.run
+
+#Configure tint2
+echo "tint2 &" >> ~/.config/openbox/autostart
