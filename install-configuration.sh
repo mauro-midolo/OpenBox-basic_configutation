71f6c5d8423a1caa5b450b0a9dec05ceb08a5f8a
Mauro <mmxx91@hotmail.it>
2020-02-14 13:07:21 +0100

    Removed Chrome, added Chromium-browser

--- install-configuration.sh
+++ install-configuration.sh
@@ -3,13 +3,8 @@
 #install basic application
 sudo apt-get install -y openbox obmenu obconf xinit \
 	terminator conky unzip nautilus gcc make tint2 \
-	network-manager-gnome
-
-#Install Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
-
+	network-manager-gnome chromium-browser
+	
 #install Nvidia Driver
 wget http://it.download.nvidia.com/XFree86/Linux-x86_64/340.108/NVIDIA-Linux-x86_64-340.108.run
 chmod +x NVIDIA-Linux-x86_64-340.108.run
